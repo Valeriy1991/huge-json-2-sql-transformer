a8d691f05628a9e9d5f21258cd4b00e70181b490
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2019-07-17 15:05:14 +0300

    Add JSON file reader

---
+++ HugeJson2SqlTransformer/Json/JsonFileReader.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using HugeJson2SqlTransformer.Json.Abstract;
+
+namespace HugeJson2SqlTransformer.Json
+{
+    public class JsonFileReader : IJsonFileReader
+    {
+        public Task<string> ReadAllTextAsync(string jsonFilePath)
+        {
+            return File.ReadAllTextAsync(jsonFilePath);
+        }
+    }
+}
